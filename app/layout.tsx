45f3165adfd14d39c3ac5c9292d84412cbba1811
Gusteffens <gufrankesteffens@gmail.com>
2026-07-15 18:25:36 -0300

    feat: move Meta Pixel code to head section of page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,6 +101,35 @@ export default function RootLayout({
             <head>
                 <link rel="preconnect" href="https://clerk.ehsadvogados.com.br" />
                 <JsonLd />
+                {/* Meta Pixel Code */}
+                <Script
+                    id="meta-pixel"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            !function(f,b,e,v,n,t,s)
+                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                            n.queue=[];t=b.createElement(e);t.async=!0;
+                            t.src=v;s=b.getElementsByTagName(e)[0];
+                            s.parentNode.insertBefore(t,s)}(window, document,'script',
+                            'https://connect.facebook.net/en_US/fbevents.js');
+                            fbq('init', '1505326658290342');
+                            fbq('track', 'PageView');
+                        `,
+                    }}
+                />
+                <noscript>
+                    <img 
+                        height="1" 
+                        width="1" 
+                        style={{ display: "none" }}
+                        src="https://www.facebook.com/tr?id=1505326658290342&ev=PageView&noscript=1"
+                        alt=""
+                    />
+                </noscript>
+                {/* End Meta Pixel Code */}
             </head>
             <body className="antialiased grain-overlay">
                 {/* Google Tag Manager (noscript) */}
@@ -142,33 +171,7 @@ export default function RootLayout({
                         `,
                     }}
                 />
-                 <Script
-                    id="meta-pixel"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            !function(f,b,e,v,n,t,s)
-                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                            n.queue=[];t=b.createElement(e);t.async=!0;
-                            t.src=v;s=b.getElementsByTagName(e)[0];
-                            s.parentNode.insertBefore(t,s)}(window, document,'script',
-                            'https://connect.facebook.net/en_US/fbevents.js');
-                            fbq('init', '1505326658290342');
-                            fbq('track', 'PageView');
-                        `,
-                    }}
-                />
-                <noscript>
-                    <img 
-                        height="1" 
-                        width="1" 
-                        style={{ display: "none" }}
-                        src="https://www.facebook.com/tr?id=1505326658290342&ev=PageView&noscript=1"
-                        alt=""
-                    />
-                </noscript>
+
                 <ClerkProvider signInUrl="/sign-in" signUpUrl="/sign-up">
                     <PostHogProvider>
                         <SiteShell>{children}</SiteShell>
